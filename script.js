60cfb1e6f61d43257ef3e30441bf7c378c8a88b9
svoja <switzersawet@gmail.com>
2024-11-04 03:17:14 +0700

    Imma just sleep

--- script.js
+++
@@ -1,8 +0,0 @@
-liff.init({ liffId: "2006526744-9zLD3PAm" })
-    .then(() => {
-        console.log("LIFF initialized");
-        // Further actions after initialization
-    })
-    .catch((err) => {
-        console.error("LIFF initialization failed", err);
-    });
